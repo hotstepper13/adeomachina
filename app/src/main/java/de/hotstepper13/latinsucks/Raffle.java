8c2399eaa3f0d6a614cfd3ee560ebec08b7b274f
Frank Mueller <fmueller@sapient.com>
2017-09-07 20:25:09 +0200

    Initial

---
+++ app/src/main/java/de/hotstepper13/latinsucks/Raffle.java
@@ -0,0 +1,13 @@
+package de.hotstepper13.latinsucks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Raffle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raffle);
+    }
+}
